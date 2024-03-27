630493641e0a923514000e9c593b77bba2814d4a
Brittany Wong <brittanywong@live.ca>
2024-03-26 21:34:42 -0400

    Cleanup: Added comments

--- scripts/get-changed-packages.sh
+++ scripts/get-changed-packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+# gets the newline separated names of the changed packages between the
+# base branch and the internal merge commit
 
-# Run git diff to get the changed packages between the base branch and the internal merge commit
 CHANGED_PACKAGES=$(git diff $GITHUB_BASE_REF $GITHUB_SHA --name-only |
                   grep -o "\bpackages/[^/]\+\b" | sed "s/packages\///" | uniq)
 
-# Output the changed packages
 echo "$CHANGED_PACKAGES"
